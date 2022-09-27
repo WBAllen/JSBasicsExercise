fd3fe33c7606754153d423ffd121c3303eca0e67
Ben Allen <live.live.live.island@gmail.com>
2022-09-27 10:37:03 -0400

    JSbasicsEx

---
+++ script.js
@@ -0,0 +1,51 @@
+// 1
+console.log(888);
+// 2
+// Numbers, Strings, Booleans, Null, Undefined
+// 3
+// (Addition)+ (Subtraction)- (Multiplication)* (Division)/ (Exponentiation)** (Modulus)%
+// EXTRA (Increment)++ (Decrement)--
+
+// 4
+console.log(5 % 4);
+console.log(5 % 2);
+// 5
+console.log('I\'M MARY POPPINS, Y\'ALL');
+// 6
+console.log("Hawk" + "eye");
+// 7
+console.log("Ben ".length + "Allen".length);
+// 8
+console.log("Avengers: Endgame"[16]);
+
+// BONUS
+// 1
+console.log("\\");
+// 2
+console.log(5 ** 4 - 9);
+// 3
+console.log(("I".length + "Am".length + "Iron".length + "Man".length) % 10);
+// 4
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+// 5
+// var m=25;
+// var c=4;
+// var u = m + c
+// console.log(u);
+
+// const m=25;
+// const c=4;
+// const u = m + c
+// console.log(u);
+
+let m=25;
+let c=4;
+let u = m + c
+console.log(u);
